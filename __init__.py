276c52c1091eeb96f4b3d1cd2386e99bfcd6e12c
Ceyase <jjcyf@foxmail.com>
2025-01-25 19:57:40 +0800

    Auto Build & Style Update

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ from main_window import LocalizationIDE
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    app.setStyle("windowsvista")
     window = LocalizationIDE()
     window.show()
     sys.exit(app.exec())
